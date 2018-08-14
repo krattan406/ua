7d02adf3a00e78a66c0243eb627d11df243fafb1
krattan406 <30876207+krattan406@users.noreply.github.com>
2018-08-13 23:13:00 -0700

    Update list.js
    Fixed dynamodb index

--- chat-service/chats/list.js
+++ chat-service/chats/list.js
@@ -1,31 +1,35 @@
 'use strict';
-
 const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
   
 module.exports.list = (event, context, callback) => {
- let user_name = event.username;
- console.log("user_name " + user_name);
+  let user_name = event.username;
+  console.log("user_name " + user_name);
   
   var params = {
-     TableName: process.env.DYNAMODB_TABLE,
-    FilterExpression: 'contains(username, user_name)',
+    TableName: process.env.DYNAMODB_TABLE,
+   // FilterExpression: 'username = ' + event.username,
+    ProjectionExpression: "id, username"
   };
   
- console.log("params " + params);
- // fetch all chat for a username from the database
-  dynamoDb.scan(params, function(err, data) {
-    if (err) {
-        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-    } else {
-        console.log("Query succeeded.");
-        console.log('Count =',data.Items.length);
+  console.log("Scanning chat table.");
+    const onScan = (err, data) => {
 
-        if (data.Items.length > 0){
-            console.log("post exists in tag");
-        }else{
-            console.log("post doesnt exist in tag");
+        if (err) {
+            console.log('Scan failed to load data. Error JSON:', JSON.stringify(err, null, 2));
+            callback(err);
+        } else {
+            console.log("Scan succeeded." +data.Items);
+            return callback(null, {
+                statusCode: 200,
+                body: JSON.stringify({
+                    "": data.Items
+                })
+            });
         }
-    }
-});
-}
+
+    };
+
+    dynamoDb.scan(params, onScan);
+
+};
