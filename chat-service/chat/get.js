84b9606e1e248b3719fd84103379bc5951b9a18f
krattan406 <30876207+krattan406@users.noreply.github.com>
2018-08-13 21:33:06 -0700

    Update get.js
    fixed response vars

--- chat-service/chat/get.js
+++ chat-service/chat/get.js
@@ -5,11 +5,16 @@ const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-depe
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.get = (event, context, callback) => {
+  const reqx = console.log(event.body);
+  const id = event.id;
+  
+  
   const params = {
     TableName: process.env.DYNAMODB_TABLE,
     Key: {
-      id: event.pathParameters.id,
+      id: id,
     },
+    
   };
 
   // fetch chatfrom the database
@@ -27,9 +32,15 @@ module.exports.get = (event, context, callback) => {
 
     // create a response
     const response = {
-      statusCode: 200,
-      body: JSON.stringify(result.Item),
+     statusCode: 200,
+          body: JSON.stringify(
+                { 'username' :  result.Item.username,
+                 'text' : result.Item.text,
+                 'expiration_date':  result.Item.timeout
+                }
+       )
     };
+    console.log(response);
     callback(null, response);
   });
 };
